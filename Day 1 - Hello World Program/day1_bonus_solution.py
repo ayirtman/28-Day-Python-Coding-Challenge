459c61d6eacdeb3413e3c926e01549288fdd5c20
Batu Ayirtman <batuayirtman@gmail.com>
2023-06-19 13:10:45 +0300

    Add bonus solution file and Update README file

---
+++ Day 1 - Hello World Program/day1_bonus_solution.py
@@ -0,0 +1,20 @@
+# Day 1 Bonus Challenge: Greeting User
+
+# Ask for the user's name
+name = input("Please enter your name: ")
+
+# Ask for the user's age
+age = input("Please enter your age: ")
+
+# Ask for the user's favorite color
+color = input("Please enter your favorite color: ")
+
+# Bonus: Ask for the user's country of residence
+country = input("Please enter your country of residence: ")
+
+# Bonus: Ask for the user's favorite food
+food = input("Please enter your favorite food: ")
+
+# Print a greeting that includes the user's details
+print(f"Hello, {name}! You are {age} years old and your favorite color is {color}.")
+print(f"You live in {country} and your favorite food is {food}.")
