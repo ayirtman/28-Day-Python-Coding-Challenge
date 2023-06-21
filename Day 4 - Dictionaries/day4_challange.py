dcb4bcdfe32fb7c5fe84b9e14c14746074db67a4
Batu Ayirtman <batuayirtman@gmail.com>
2023-06-21 15:49:13 +0300

    Add Day 4 challenge files

---
+++ Day 4 - Dictionaries/day4_challange.py
@@ -0,0 +1,16 @@
+# Day 4 Challenge: Phonebook Application
+
+# Your task is to build a simple phonebook application using dictionaries.
+# Users should be able to add, remove, and look up contacts by name,
+# and the contact details should include name and phone number.
+
+# Initialize an empty dictionary for the phonebook
+# TODO: write your code here
+
+# Define functions to add, remove, and look up contacts
+# TODO: write your code here
+
+# Start a loop for user interaction
+# TODO: write your code here
+
+# Bonus extend your phonebook application to support additional contact details, such as email address.
